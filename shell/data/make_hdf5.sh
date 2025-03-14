5701511f4c55ab0953ec0017070fa1d94adfbe43
German <yageraaa@gmail.com>
2025-03-14 22:37:27 +0300

    Update make_hdf5.sh for 250x250 dataset
    - Changed HW to 250 to match dataset dimensions.
    - Updated CT and MR file names to CT.nii.gz and MRI.nii.gz.
    - Removed hardcoded paths and replaced with relative paths.

--- shell/data/make_hdf5.sh
+++ shell/data/make_hdf5.sh
@@ -1,25 +1,29 @@
+#!/bin/bash
 
-for HW in 160 
+HW=250
+CT_name="CT.nii.gz" #поменятье
+MR_name="MRI.nii.gz" #поменять
+data_dir="../" #поменять
+out_dir="../" #поменять
+log_dir="../logs/hdf5"
+
+mkdir -p $out_dir $log_dir
+
+for which in "train" "valid" "test"
 do
-    CT_name="CT_preprocessed_$HW.nii"
-    MR_name="MR_preprocessed_$HW.nii"
-
-    for which in "train" "valid" "test"
+    for plane in "axial"
     do
-        # for plane in "axial" "sagittal" "coronal"
-        for plane in "axial"
-        do
         python -u brain_dataset_utils/generate_total_hdf5_csv.py \
-                --plane  $plane\
-                --which_set $which \
-                --height $HW \
-                --width $HW \
-                --hdf5_name "/root_dir/datasets/${HW}_${which}_${plane}.hdf5" \
-                --data_dir "/root_dir/datasets/raw_data" \
-                --data_csv "/root_dir/datasets/dataset_split.csv" \
-                --CT_name $CT_name \
-                --MR_name $MR_name \
-                > /root_dir/datasets/hdf5_log/${HW}_${which}_${plane}.log
-        done      
+            --plane $plane \
+            --which_set $which \
+            --height $HW \
+            --width $HW \
+            --hdf5_name "$out_dir/${HW}_${which}_${plane}.hdf5" \
+            --data_dir $data_dir \
+            --data_csv "" \ #поменять если есть csv
+            --CT_name $CT_name \
+            --MR_name $MR_name \
+            > "$log_dir/${HW}_${which}_${plane}.log"
     done
-done      
+done
+
