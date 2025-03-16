3d5c366a01b74e7b93c8f8d4faf054f482b1bee6
German <yageraaa@gmail.com>
2025-03-16 18:56:06 +0300

    Update make_hdf5.sh to use relative paths

--- shell/data/make_hdf5.sh
+++ shell/data/make_hdf5.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 HW=250
-CT_name="CT.nii.gz" #поменятье
-MR_name="MRI.nii.gz" #поменять
-data_dir="../" #поменять
-out_dir="../" #поменять
-log_dir="../logs/hdf5"
+CT_name="ct.nii.gz"
+MR_name="mr.nii.gz"
+data_dir="../../data"
+out_dir="../../processed_data"
+log_dir="../../logs/hdf5"
 
-mkdir -p $out_dir $log_dir
+mkdir -p "$out_dir" "$log_dir"
 
 for which in "train" "valid" "test"
 do
-    for plane in "axial"
-    do
-        python -u brain_dataset_utils/generate_total_hdf5_csv.py \
-            --plane $plane \
-            --which_set $which \
-            --height $HW \
-            --width $HW \
-            --hdf5_name "$out_dir/${HW}_${which}_${plane}.hdf5" \
-            --data_dir $data_dir \
-            --data_csv "" \ #поменять если есть csv
-            --CT_name $CT_name \
-            --MR_name $MR_name \
-            > "$log_dir/${HW}_${which}_${plane}.log"
-    done
+    if [ -d "${data_dir}/${which}" ]; then
+        for plane in "axial"
+        do
+            python -u ../brain_dataset_utils/generate_total_hdf5_csv.py \
+                --plane "$plane" \
+                --which_set "$which" \
+                --height "$HW" \
+                --width "$HW" \
+                --hdf5_name "${out_dir}/${HW}_${which}_${plane}.hdf5" \
+                --data_dir "${data_dir}/${which}" \
+                --data_csv "" \
+                --CT_name "$CT_name" \
+                --MR_name "$MR_name" \
+                > "${log_dir}/${HW}_${which}_${plane}.log"
+        done
+    else
+        echo "Skipping ${which} set: directory not found."
+    fi
 done
-
