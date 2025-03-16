23cda1f912013a1bc5c375d8d65e5909de328d79
German <yageraaa@gmail.com>
2025-03-16 19:08:47 +0300

    Update test.sh to use relative paths

--- shell/test/test.sh
+++ shell/test/test.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
-cd /root_dir/code
+cd "$(dirname "$0")/.."
 
 config_name="BBDM_base.yaml"
-HW="160"
+HW=250
 plane="axial"
 ddim_eta=0.0
-
 gpu_ids="0"
 
-exp_name="241213_160_BBDM_axial_DDIM_MR_global_hist_context"
-
-# test
+exp_name="241213_250_BBDM_axial_DDIM_MR_global_hist_context"
 test_epoch="34"
-resume_model="/root_dir/code/results/ct2mr_$HW/$exp_name/checkpoint/latest_model_$test_epoch.pth"
-resume_optim="/root_dir/code/results/ct2mr_$HW/$exp_name/checkpoint/latest_optim_sche_$test_epoch.pth"
 
-sample_step=100
-inference_type="normal" # normal, average, ISTA_average, ISTA_mid
-ISTA_step_size=0.5
-num_ISTA_step=1
+resume_model="../results/ct2mr_${HW}/${exp_name}/checkpoint/latest_model_${test_epoch}.pth"
+resume_optim="../results/ct2mr_${HW}/${exp_name}/checkpoint/latest_optim_sche_${test_epoch}.pth"
 
-python /root_dir/code/main.py \
-    --exp_name $exp_name \
-    --config /root_dir/code/results/ct2mr_$HW/$exp_name/checkpoint/config_backup.yaml \
+python main.py \
+    --exp_name "$exp_name" \
+    --config "configs/${config_name}" \
     --sample_to_eval \
-    --gpu_ids $gpu_ids \
-    --resume_model $resume_model \
-    --resume_optim $resume_optim \
-    --HW $HW \
-    --plane $plane \
-    --ddim_eta $ddim_eta \
-    --sample_step $sample_step \
-    --inference_type $inference_type \
-    --ISTA_step_size $ISTA_step_size \
-    --num_ISTA_step $num_ISTA_step
-
-
+    --gpu_ids "$gpu_ids" \
+    --resume_model "$resume_model" \
+    --resume_optim "$resume_optim" \
+    --HW "$HW" \
+    --plane "$plane" \
+    --ddim_eta "$ddim_eta"
