5355bf6d770184951cc8215af22042c78365d12c
German <yageraaa@gmail.com>
2025-03-16 19:01:19 +0300

    Update make_hist_dataset.sh to use relative paths

--- shell/data/make_hist_dataset.sh
+++ shell/data/make_hist_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 HW=250
-CT_name="CT.nii.gz" #поменять
-MR_name="MRI.nii.gz" #поменять
-data_dir="../" #поменять
-out_dir="../" #поменять
-log_dir="../logs/hist"
+CT_name="ct.nii.gz"
+MR_name="mr.nii.gz"
+data_dir="../../data"
+out_dir="../../processed_data"
+log_dir="../../logs/hist"
 
-mkdir -p $out_dir $log_dir
+mkdir -p "$out_dir" "$log_dir"
 
 for which in "train" "valid" "test"
 do
@@ -15,18 +15,18 @@ do
     do
         for hist_type in "normal" "avg" "colin"
         do
-            python -u brain_dataset_utils/generate_total_hist_global.py \
-                --plane $plane \
-                --hist_type $hist_type \
-                --which_set $which \
-                --height $HW \
-                --width $HW \
-                --pkl_name "$out_dir/MR_hist_global_${HW}_${which}_${plane}_${hist_type}.pkl" \
-                --data_dir $data_dir \
-                --data_csv "" \ #поменять
-                --CT_name $CT_name \
-                --MR_name $MR_name \
-                > "$log_dir/MR_hist_global_${HW}_${which}_${plane}_${hist_type}.log"
+            python -u ../brain_dataset_utils/generate_total_hist_global.py \
+                --plane "$plane" \
+                --hist_type "$hist_type" \
+                --which_set "$which" \
+                --height "$HW" \
+                --width "$HW" \
+                --pkl_name "${out_dir}/MR_hist_global_${HW}_${which}_${plane}_${hist_type}.pkl" \
+                --data_dir "${data_dir}/${which}" \
+                --data_csv "" \
+                --CT_name "$CT_name" \
+                --MR_name "$MR_name" \
+                > "${log_dir}/MR_hist_global_${HW}_${which}_${plane}_${hist_type}.log"
         done
     done
 done
