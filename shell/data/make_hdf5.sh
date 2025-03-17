bfaa5e143ed8fb63ab7d2d1c627e041708fc73f2
German <yageraaa@gmail.com>
2025-03-17 22:37:08 +0300

    Update script paths for correct functionality

--- shell/data/make_hdf5.sh
+++ shell/data/make_hdf5.sh
@@ -3,9 +3,9 @@
 HW=250
 CT_name="ct.nii.gz"
 MR_name="mr.nii.gz"
-data_dir="../data"
-out_dir="../../processed_data"
-log_dir="../../logs/hdf5"
+data_dir="./data"
+out_dir="./processed_data"
+log_dir="./logs/hdf5"
 
 mkdir -p "$out_dir" "$log_dir"
 
@@ -14,14 +14,13 @@ do
     if [ -d "${data_dir}/${which}" ]; then
         for plane in "axial"
         do
-            python -u ../brain_dataset_utils/generate_total_hdf5_csv.py \
+            python -u ./brain_dataset_utils/generate_total_hdf5_csv.py \
                 --plane "$plane" \
                 --which_set "$which" \
                 --height "$HW" \
                 --width "$HW" \
                 --hdf5_name "${out_dir}/${HW}_${which}_${plane}.hdf5" \
                 --data_dir "${data_dir}/${which}" \
-                --data_csv "" \
                 --CT_name "$CT_name" \
                 --MR_name "$MR_name" \
                 > "${log_dir}/${HW}_${which}_${plane}.log" 2>&1
--- shell/data/make_hist_dataset.sh
+++ shell/data/make_hist_dataset.sh
@@ -3,9 +3,9 @@
 HW=250
 CT_name="ct.nii.gz"
 MR_name="mr.nii.gz"
-data_dir="../../data"
-out_dir="../../processed_data"
-log_dir="../../logs/hist"
+data_dir="./data"
+out_dir="./processed_data"
+log_dir="./logs/hdf5"
 
 mkdir -p "$out_dir" "$log_dir"
 
@@ -16,7 +16,7 @@ do
         do
             for hist_type in "normal" "avg" "colin"
             do
-                python -u ../brain_dataset_utils/generate_total_hist_global.py \
+                python -u ./brain_dataset_utils/generate_total_hist_global.py \
                     --plane "$plane" \
                     --hist_type "$hist_type" \
                     --which_set "$which" \
@@ -24,7 +24,6 @@ do
                     --width "$HW" \
                     --pkl_name "${out_dir}/MR_hist_global_${HW}_${which}_${plane}_${hist_type}.pkl" \
                     --data_dir "${data_dir}/${which}" \
-                    --data_csv "" \
                     --CT_name "$CT_name" \
                     --MR_name "$MR_name" \
                     > "${log_dir}/MR_hist_global_${HW}_${which}_${plane}_${hist_type}.log" 2>&1
